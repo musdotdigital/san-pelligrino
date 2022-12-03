c53f3ed7b9d6abbb4ad9007a18cec8bcb48ce543
mus <alquraishimustafa@gmail.com>
2022-12-03 18:19:02 +0000

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -153,56 +153,47 @@ function Home() {
                     </div>
                 </div>
 
-                <TwoColumnFeature
-                    layout="2/3"
-                    columnOne={
-                        <div className=" bg-gray-200 rounded-lg overflow-hidden drop-shadow-lg">
-                            <img
-                                alt="/slide-one.png"
-                                src={slideOne}
-                                className="group-hover:opacity-75"
-                                loading="lazy"
-                            />
-                        </div>
-                    }
-                    columnTwo={
-                        <div className="col-span-1">
-                            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:mt-36">
-                                Ready to level up your research game?
-                            </h2>
-                            <p className="mt-6 text-lg leading-8 text-gray-600 ">
-                                The platform to search for papers, organise your research and
-                                collaborate. All in one place. We handle the boring stuff so you can
-                                focus on what matters.
-                            </p>
-                        </div>
-                    }
-                />
-                <TwoColumnFeature
-                    layout="1/2"
-                    columnOne={
-                        <div className=" bg-gray-200 rounded-lg overflow-hidden drop-shadow-lg">
-                            <img
-                                alt="/slide-one.png"
-                                src={slideOne}
-                                className="group-hover:opacity-75"
-                                loading="lazy"
-                            />
-                        </div>
-                    }
-                    columnTwo={
-                        <div className="col-span-1">
-                            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:mt-36">
-                                Ready to level up your research game?
-                            </h2>
-                            <p className="mt-6 text-lg leading-8 text-gray-600 ">
-                                The platform to search for papers, organise your research and
-                                collaborate. All in one place. We handle the boring stuff so you can
-                                focus on what matters.
-                            </p>
-                        </div>
-                    }
-                />
+                <TwoColumnFeature layout="2/3">
+                    <div className=" bg-gray-200 rounded-lg overflow-hidden drop-shadow-lg">
+                        <img
+                            alt="/slide-one.png"
+                            src={slideOne}
+                            className="group-hover:opacity-75"
+                            loading="lazy"
+                        />
+                    </div>
+                    <div className="col-span-1">
+                        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:mt-36">
+                            Ready to level up your research game?
+                        </h2>
+                        <p className="mt-6 text-lg leading-8 text-gray-600 ">
+                            The platform to search for papers, organise your research and
+                            collaborate. All in one place. We handle the boring stuff so you can
+                            focus on what matters.
+                        </p>
+                    </div>
+                </TwoColumnFeature>
+
+                <TwoColumnFeature layout="1/2">
+                    <div className=" bg-gray-200 rounded-lg overflow-hidden drop-shadow-lg">
+                        <img
+                            alt="/slide-one.png"
+                            src={slideOne}
+                            className="group-hover:opacity-75"
+                            loading="lazy"
+                        />
+                    </div>
+                    <div className="col-span-1">
+                        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:mt-36">
+                            Ready to level up your research game?
+                        </h2>
+                        <p className="mt-6 text-lg leading-8 text-gray-600 ">
+                            The platform to search for papers, organise your research and
+                            collaborate. All in one place. We handle the boring stuff so you can
+                            focus on what matters.
+                        </p>
+                    </div>
+                </TwoColumnFeature>
 
                 <div className="bg-gray-50">
                     <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8 ">
